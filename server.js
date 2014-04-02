eb892948df43caa95e57d44e0323f089da594e8c
Andreas Kirn <akirn@switcheye.com>
2014-04-02 13:03:30 -0700

    Fixing routes

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-// server.js
+'use strict';
 
-// modules =================================================
 var express = require('express');
-var app     = express();
-var mongoose= require('mongoose');
+var app = express();
+//var mongoose = require('mongoose');
 
-// configuration ===========================================
-
-// config files
 var db = require('./config/db');
 
-var port = process.env.PORT || 8080; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
+var port = process.env.PORT || 5000;
+// mongoose.connect(db.url);
 
 app.configure(function() {
-    app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
-    app.use(express.logger('dev'));                     // log every request to the console
-    app.use(express.bodyParser());                      // have the ability to pull information from html in POST
-    app.use(express.methodOverride());                  // have the ability to simulate DELETE and PUT
-});
+    //app.use('/public', express.static(__dirname + '/public'));
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+  });
 
-// routes ==================================================
-require('./app/routes')(app); // configure our routes
+require('./app/routes')(app);
 
-// start app ===============================================
-app.listen(port);                                       // startup our app at http://localhost:8080
-console.log('Magic happens on port ' + port);           // shoutout to the user
-exports = module.exports = app;                         // expose app
+app.listen(port);
+console.log('Connected on port ' + port);
+exports = module.exports = app;
