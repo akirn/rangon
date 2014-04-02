4a5f0af83df205a2b76360a70d324abcd1a070b8
Andreas Kirn <akirn@switcheye.com>
2014-04-02 13:03:13 -0700

    Adding routes

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,20 @@
 
 module.exports = function(app) {
 
+  app.get('/', function(req, res) {
+      res.sendfile('/views/index.html', {root: './public'});
+    });
+
+  app.get('/js/:file', function(req, res) {
+      res.sendfile('/js/' + req.params.file, {root: './public'});
+    });
+
+  app.get('/css/:file', function(req, res) {
+      res.sendfile('/css/' + req.params.file, {root: './public'});
+    });
+
   app.get('*', function(req, res) {
-    res.sendfile('./public/views/index.html');
-  });
+      res.send(404, 'File not found.');
+    });
 
 };
