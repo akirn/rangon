2c8134bd5b976246539b527678e79c13ea5a23b8
Andreas Kirn <akirn@switcheye.com>
2014-04-02 22:30:30 -0700

    Consolidating static routes

--- app/routes.js
+++ app/routes.js
@@ -2,20 +2,29 @@
 
 module.exports = function(app) {
 
+  var errorout = function(res) {
+    res.status(404).sendfile('/views/404.html', opts);
+  };
+
+  var opts = {root: './public'};
+
   app.get('/', function(req, res) {
-      res.sendfile('/views/index.html', {root: './public'});
+      res.sendfile('/views/index.html', opts);
     });
 
-  app.get('/js/:file', function(req, res) {
-      res.sendfile('/js/' + req.params.file, {root: './public'});
-    });
+  app.get('/:dir/:file', function(req, res, next) {
+      var path = '/' + req.params.dir + '/' + req.params.file;
 
-  app.get('/css/:file', function(req, res) {
-      res.sendfile('/css/' + req.params.file, {root: './public'});
+      res.sendfile(path, opts, function(err) {
+        if (err.code === 'ENOENT') {
+          return next();
+        }
+        res.send(500);
+      });
     });
 
   app.get('*', function(req, res) {
-      res.status(404).sendfile('/views/404.html', {root: './public'});
+      res.status(404).sendfile('/views/404.html', opts);
     });
 
 };
